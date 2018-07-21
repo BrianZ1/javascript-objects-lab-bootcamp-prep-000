b8ae4e0e8ca859c80f3c12656f4ec46822ea4b7c
Brian Zhang <brianzhangw@gmail.com>
2018-07-21 02:32:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var new_object = 
+  var new_object = Object.assign({}, object);
+  
+  delete new_object.key;
+  return new_object;
+  
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-    delete object.key
-    return object
+    delete object.key;
+    return object;
 }
